1084267d890c4d4216cd4598ca6ffc412acf344f
47993849w <47993849w>
2015-11-08 19:35:19 +0100

    Adaptador listo

--- app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
+++ app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -19,7 +26,17 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        View pelis = rootView.findViewById(R.id.pelis);
+        ListView pelis = (ListView)rootView.findViewById(R.id.pelis);
+
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+            R.layout.ly_pelis,
+             R.id.tvPelis,
+            items
+                );
+        pelis.setAdapter(adapter);
         return rootView;
     }
 }
