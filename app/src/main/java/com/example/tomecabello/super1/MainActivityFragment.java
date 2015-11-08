e146a8719e29216957ebf570ccc66943f80d3d36
Adrià Tomé C <Adri Tom C>
2015-11-08 19:17:27 +0100

    Initial commit (eliminado icono sms, creado el LISTVIEW!)

---
+++ app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package com.example.tomecabello.super1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View pelis = rootView.findViewById(R.id.pelis);
+        return rootView;
+    }
+}
