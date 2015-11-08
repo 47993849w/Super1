cf9476fb2e71a337b30c9398225a50b4a7769152
47993849w <47993849w>
2015-11-08 21:30:22 +0100

    Creado el servicio + creado interficie

--- app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
+++ app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
@@ -2,6 +2,7 @@ package com.example.tomecabello.super1;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,16 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.example.tomecabello.super1.json.API;
+import com.example.tomecabello.super1.json.Result;
+
+import retrofit.Call;
+import retrofit.Callback;
 import retrofit.GsonConverterFactory;
+import retrofit.Response;
 import retrofit.Retrofit;
+import retrofit.http.GET;
+import retrofit.http.Query;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -35,6 +44,7 @@ public class MainActivityFragment extends Fragment {
 
 
 
+        System.out.println("dddddddddddddddd");
         String[] data = {
                 "Prueba 1",
                 "Prueba 2",
@@ -48,11 +58,43 @@ public class MainActivityFragment extends Fragment {
             items
                 );
         pelis.setAdapter(adapter);
-        final String BASE_URL = "https://api.themoviedb.org/3/movie/";
+        final String API_KEY = "6cb54438ece271e5a26d8c532fac02ce";
+        final String BASE_URL = "https://api.themoviedb.org/3/";
         Retrofit retrofit = new Retrofit.Builder()
                               .baseUrl(BASE_URL)
                                .addConverterFactory(GsonConverterFactory.create())
                                .build();
+
+        Interface servei = retrofit.create(Interface.class);
+
+        Call<API> call = servei.getPeliculesMesVistes();
+        call.enqueue(new Callback<API>() {
+
+            @Override
+            public void onResponse(Response<API> response, Retrofit retrofit) {
+                if (response.isSuccess()) {
+                        Log.d(null, "OK");
+                    API api= response.body();
+                    System.out.println("BASURAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                }
+
+            }
+
+            @Override
+                       public void onFailure(Throwable t) {
+                System.out.println("BASURAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAArfewqaf34erAAAAAAAAAAAAA");
+
+            }
+        });
+
         return rootView;
+
     }
+
+
+
+    public interface Interface {
+        @GET("discover/movie?sort_by=popularity.desc&api_key=6cb54438ece271e5a26d8c532fac02ce")
+        Call<API> getPeliculesMesVistes();
+            }
 }
