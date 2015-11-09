c5f61da033a8bf9a0e35392e94ae525680370b35
47993849w <47993849w>
2015-11-09 02:20:52 +0100

    escribo onStart

--- app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
+++ app/src/main/java/com/example/tomecabello/super1/MainActivityFragment.java
@@ -30,6 +30,12 @@ public class MainActivityFragment extends Fragment {
             }
 
 
+    public void onStart() {
+                super.onStart();
+                refresh();
+            }
+
+
 
 
 
